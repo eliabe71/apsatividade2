2cc989d36b1a893f0708be0c18f82b602140317d
AndersonSilvaUFC <andersonsilva@alu.ufc.br>
2020-12-10 15:50:43 -0300

    Adicionando Location

--- src/enties/repositories/Location.java
+++ src/enties/repositories/Location.java
@@ -1,5 +1,79 @@
 package enties.repositories;
 
+import java.util.Calendar;
+import java.util.Date;
+
+
 public class Location {
-    
+    private int codigoProduto;
+    private int matriculaCliente;
+    private Calendar dataSaida;
+    private Calendar dataPrevistaEntrega;
+
+
+    public Location(){}
+
+    public Location(int codigoProduto, int matriculaCliente, Calendar dataSaida, Calendar dataPrevistaEntrega){
+        this.codigoProduto = codigoProduto;
+        this.matriculaCliente = matriculaCliente;
+        this.dataSaida = dataSaida;
+        this.dataPrevistaEntrega = dataPrevistaEntrega;
+    }
+
+    public double calcularMulta(){
+        Date dataAtual = new Date(System.currentTimeMillis());
+        int diferenca = diffInDays(dataPrevistaEntrega.getTime(), dataAtual);
+
+        if(diferenca > 0)
+            return diferenca*1.2;
+
+        return 0;
+    }
+
+    public int getCodigoProduto(){
+        return this.codigoProduto;
+    }
+    public void setCodigoProduto(int codigoProduto){
+        this.codigoProduto = codigoProduto;
+    }
+
+    public int getMatriculaCliente(){
+        return this.matriculaCliente;
+    }
+    public void setMatriculaCliente(int matriculaCliente){
+        this.matriculaCliente = matriculaCliente;
+    }
+
+    public Calendar getDataSaida(){
+        return this.dataSaida;
+    }
+    public void setDataSaida(Calendar dataSaida){
+        this.dataSaida = dataSaida;
+    }
+
+    public Calendar getDataPrevistaEntrega(){
+        return this.dataPrevistaEntrega;
+    }
+    public void setDataPrevistaEntrega(Calendar dataPrevistaEntrega){
+        this.dataPrevistaEntrega = dataPrevistaEntrega;
+    }
+
+    public static int diffInDays(Date date1, Date date2) {
+		int MILLIS_IN_DAY = 86400000;
+
+		Calendar c1 = Calendar.getInstance();
+		c1.setTime(date1);
+		c1.set(Calendar.MILLISECOND, 0);
+		c1.set(Calendar.SECOND, 0);
+		c1.set(Calendar.MINUTE, 0);
+		c1.set(Calendar.HOUR_OF_DAY, 0);
+
+		Calendar c2 = Calendar.getInstance();
+		c2.setTime(date2);
+		c2.set(Calendar.MILLISECOND, 0);
+		c2.set(Calendar.SECOND, 0);
+		c2.set(Calendar.MINUTE, 0);
+		c2.set(Calendar.HOUR_OF_DAY, 0);
+		return (int) ((c1.getTimeInMillis() - c2.getTimeInMillis()) / MILLIS_IN_DAY);
+	}
 }
--- src/enties/repositories/Teste.java
+++ src/enties/repositories/Teste.java
@@ -1,7 +1,17 @@
 package enties.repositories;
 
+import java.util.Calendar;
+import java.util.Date;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+
 public class Teste{
     public static void main(String[] args){
-        
+        Calendar d1 = Calendar.getInstance();
+        Calendar d2 = Calendar.getInstance();
+        d2.set(2020, 11, 12);
+
+        Location l = new Location(1,3,d1,d2); 
+        System.out.println(l.calcularMulta());
     }
 }
